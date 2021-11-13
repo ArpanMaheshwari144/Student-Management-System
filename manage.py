dc491de566cc29f7f8a7201d115082e99a0650d9
Arpan Maheshwari <maheshwariarpan828@gmail.com>
2021-11-13 13:04:28 +0530

    First Commit

--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# For HOD /SuperAdmin Email: admin@gmail.com Password: admin
+# For HOD SuperAdmin Email: admin@gmail.com Password: admin
 # For Staff Email: staff@gmail.com Password: staff
 # For Student Email: student@gmail.com Password: student
 
